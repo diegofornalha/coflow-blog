f2c50be96a971e077a2a081876dc4fb713c7bdc7
Stackbit Code Editor <projects@stackbit.com>
2023-02-14 00:56:19 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,14 +5,6 @@ export default function Document() {
         <Html lang='pt-BR'>
 
             
-<link rel="alternate" hreflang="pt" href="https://coflow.com.br">
-<link rel="alternate" hreflang="en" href="https://en.coflow.com.br">
-<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
-<script>
-    Weglot.initialize({
-        api_key: 'wg_5764ae89f976da1ab84bcc66aa1f92526'
-    });
-</script>
             <Head />
             <body>
                 <Main />
