46f0204e5e006129d5c76495b88fd6c90f620509
Stackbit Code Editor <projects@stackbit.com>
2023-02-14 00:57:36 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,17 +3,6 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang='pt-BR'>
-
-<link rel="alternate" hreflang="pt" href="https://coflow.com.br">
-<link rel="alternate" hreflang="en" href="https://en.coflow.com.br">
-<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
-<script>
-    Weglot.initialize({
-        api_key: 'wg_5764ae89f976da1ab84bcc66aa1f92526'
-    });
-</script>
-
-
             <Head />
             <body>
                 <Main />
